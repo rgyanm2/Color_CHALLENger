293d98f07f85509678c0f9c7cef5caf32c75a42b
Riya G <rgyanm2@illinois.edu>
2020-04-27 18:36:10 -0500

    level1 with timer

--- app/src/main/java/com/example/colorchallenger/Level1.java
+++ app/src/main/java/com/example/colorchallenger/Level1.java
@@ -3,12 +3,28 @@ package com.example.colorchallenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class Level1 extends AppCompatActivity {
+    public int counter;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1);
+        textView = findViewById(R.id.textView5);
+        new CountDownTimer(20000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                textView.setText(String.valueOf(20 - counter));
+                counter++;
+            }
+            public void onFinish() {
+                textView.setText("0");
+            }
+        }.start();
+
     }
 }
